1dafaea739c9628e0ee74363a2224974288076a0
PedroSeber <pedroseber@hotmail.com>
2022-06-22 17:52:33 -0400

    IC: small typo fixes

--- Code-SPA/IC.py
+++ Code-SPA/IC.py
@@ -7,6 +7,7 @@ import regression_models as rm
 import numpy as np
 import nonlinear_regression as nr
 import timeseries_regression_RNN as RNN
+import pdb
 
 def IC_mse(model_name, X, y, X_test, y_test, X_val = None, y_val = None, cv_type = None, alpha_num = 50, eps = 1e-4, **kwargs):
     """
@@ -28,7 +29,7 @@ def IC_mse(model_name, X, y, X_test, y_test, X_val = None, y_val = None, cv_type
         The information criterion used for cross-validation.
         One of {'BIC', 'AIC', 'AICc'}. If None, 'AIC' or 'AICc' is used based on X data size.
     alpha_num : int, optional, default = 20
-        Penalty weight used when model_name in {'RR', 'EN', 'ALVEN', 'DALVEN', 'DALVEN_full_nonlinear'}.
+        Penalty weight used when model_name in {'DALVEN', 'DALVEN_full_nonlinear'}.
     eps : float, optional, default = 1e-4
         Tolerance. TODO: expand on this.
     **kwargs : dict, optional
@@ -45,7 +46,7 @@ def IC_mse(model_name, X, y, X_test, y_test, X_val = None, y_val = None, cv_type
         if 'l1_ratio' not in kwargs:
             kwargs['l1_ratio'] = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.97, 0.99][::-1]
         if 'degree' not in kwargs:
-            kwargs['degree'] = [1,2,3]
+            kwargs['degree'] = [1, 2, 3]
         if 'lag' not in kwargs:
             kwargs['lag'] =  [i+1 for i in range(40)]
         if 'label_name' not in kwargs:
@@ -61,8 +62,8 @@ def IC_mse(model_name, X, y, X_test, y_test, X_val = None, y_val = None, cv_type
                 for i in range(alpha_num):
                     for t in range(len(kwargs['lag'])):
                         _, _, _, _, _, _ , _, _, (AIC,AICc,BIC) = DALVEN(X, y, X_test, y_test, alpha = i, l1_ratio = kwargs['l1_ratio'][j],
-                                                      degree = kwargs['degree'][k], lag = kwargs['lag'][t], tol = eps , alpha_num = alpha_num, cv = True,
-                                                      selection = 'p_value', select_value = kwargs['select_pvalue'], trans_type = kwargs['trans_type'])
+                                                    degree = kwargs['degree'][k], lag = kwargs['lag'][t], tol = eps, alpha_num = alpha_num, cv = True,
+                                                    selection = 'p_value', select_value = kwargs['select_pvalue'], trans_type = kwargs['trans_type'])
                         if cv_type == 'AICc': # TODO: change to just = instead of += ?
                             IC_result[k,i,j,t] += AICc
                         elif cv_type == 'BIC':
@@ -76,8 +77,8 @@ def IC_mse(model_name, X, y, X_test, y_test, X_val = None, y_val = None, cv_type
         l1_ratio = kwargs['l1_ratio'][ind[2]]
         lag = kwargs['lag'][ind[3]]
         DALVEN_model, DALVEN_params, mse_train, mse_test, yhat_train, yhat_test, alpha, retain_index, _ = DALVEN(X, y, X_test, y_test, alpha = ind[1], l1_ratio = l1_ratio,
-                                                                           degree = degree, lag = lag, tol = eps, alpha_num = alpha_num, cv = False,
-                                                                           selection = 'p_value', select_value = kwargs['select_pvalue'], trans_type = kwargs['trans_type'])
+                                                                        degree = degree, lag = lag, tol = eps, alpha_num = alpha_num, cv = False,
+                                                                        selection = 'p_value', select_value = kwargs['select_pvalue'], trans_type = kwargs['trans_type'])
         hyperparams = {}
         hyperparams['alpha'] = alpha
         hyperparams['l1_ratio'] = l1_ratio
@@ -87,30 +88,19 @@ def IC_mse(model_name, X, y, X_test, y_test, X_val = None, y_val = None, cv_type
 
         # Names for the retained variables(?)
         if kwargs['label_name'] :
-            if model_name == 'DALVEN': # TODO: is there really a difference between those two? Should there be a difference?
-                if kwargs['trans_type'] == 'auto':
-                    Xtrans, _ = nr.feature_trans(X, degree = degree, interaction = 'later')
-                else:
-                    Xtrans, _ = nr.poly_feature(X, degree = degree, interaction = True, power = True)
-                # Lag padding for X
-                XD = Xtrans[lag:]
-                for i in range(lag):
-                    XD = np.hstack((XD, Xtrans[lag-1-i:-i-1]))
-            else: # DALVEN_full_nonlinear
-                # Lag padding for X
-                XD = X[lag:]
-                for i in range(lag):
-                    XD = np.hstack((XD, X[lag-1-i:-i-1]))
+            if kwargs['trans_type'] == 'auto':
+                Xtrans, _ = nr.feature_trans(X, degree = degree, interaction = 'later')
+            else:
+                Xtrans, _ = nr.poly_feature(X, degree = degree, interaction = True, power = True)
+
+            # Lag padding for X
+            XD = Xtrans[lag:]
+            for i in range(lag):
+                XD = np.hstack((XD, Xtrans[lag-1-i:-i-1]))
 
             # Lag padding for y in design matrix
             for i in range(lag):
                 XD = np.hstack((XD, y[lag-1-i:-i-1]))
-
-            if model_name == 'DALVEN_full_nonlinear': # TODO: again, is there really a difference between those two? Should there be a difference?
-                if kwargs['trans_type'] == 'auto':
-                    XD,_ = nr.feature_trans(XD, degree = degree, interaction = 'later')
-                else:
-                    XD, _ = nr.poly_feature(XD, degree = degree, interaction = True, power = True)
 
             # Remove features with insignificant variance
             sel = VarianceThreshold(threshold=eps).fit(XD)
@@ -138,8 +128,8 @@ def IC_mse(model_name, X, y, X_test, y_test, X_val = None, y_val = None, cv_type
                                 list_name_final += [f'{list_name[i]}*{list_name[j]}*{list_name[k]}']
                     list_name_final += [f'{name}^3' for name in list_name] + [f'(log{name})^3' for name in list_name] + [f'1/{name}^3' for name in list_name] + (
                                 [f'{name}^2.5' for name in list_name] + [f'(log{name})^2/{name}' for name in list_name]+ [f'log({name})/sqrt({name})' for name in list_name] +
-                                [f'log({name})/{name}^2' for name in list_name] + [f'{name}^-1.5' for name in list_name]
-                        
+                                [f'log({name})/{name}^2' for name in list_name] + [f'{name}^-1.5' for name in list_name] )
+
             elif degree >= 2:
                 for i in range(X.shape[1]):
                     for j in range(i, X.shape[1]):
@@ -149,31 +139,31 @@ def IC_mse(model_name, X, y, X_test, y_test, X_val = None, y_val = None, cv_type
                         for j in range(i, X.shape[1]):
                             for k in range(j, X.shape[1]):
                                 list_name_final += [f'{list_name[i]}*{list_name[j]}*{list_name[k]}']
-                        
+
             list_copy = list_name_final[:]
             for i in range(lag):
                 list_name_final += [f'{s}(t-{i+1})' for s in list_copy]
             for i in range(lag):
                 list_name_final += [f'y(t-{i+1})'] 
-                                
+
             index = list(sel.get_support())
             list_name_final = [x for x, y in zip(list_name_final, index) if y]
             list_name_final = [x for x, y in zip(list_name_final, retain_index) if y]
-        
+
         else:
             list_name_final =  []
         return(hyperparams, DALVEN_model, DALVEN_params, mse_train, mse_test, yhat_train, yhat_test, IC_result[ind], list_name_final)
 
     elif model_name == 'RNN':
         input_size_x = X.shape[1]
-        
+
         # Model architecture
         if 'cell_type' not in kwargs:
             kwargs['cell_type'] = ['basic']
         if 'activation' not in kwargs:
             kwargs['activation'] = ['tanh']
         if 'RNN_layers' not in kwargs:
-            kwargs['RNN_layers'] = [input_size_x]
+            kwargs['RNN_layers'] = [[input_size_x]]
 
         # Training parameters
         if 'batch_size' not in kwargs:
@@ -223,24 +213,24 @@ def IC_mse(model_name, X, y, X_test, y_test, X_val = None, y_val = None, cv_type
                                 kwargs['activation'][j], kwargs['RNN_layers'][k], kwargs['batch_size'], kwargs['epoch_overlap'], kwargs['num_steps'], kwargs['learning_rate'],
                                 kwargs['lambda_l2_reg'], kwargs['num_epochs'], kwargs['input_prob'], kwargs['output_prob'], kwargs['state_prob'], input_prob_test,
                                 output_prob_test, state_prob_test, kwargs['max_checks_without_progress'], kwargs['epoch_before_val'], kwargs['save_location'], plot = False)
-                        if cv_type == 'AICc': # TODO: change to just = instead of += ?
-                            IC_result[i,j,k] += AICc
-                        elif cv_type == 'BIC':
-                            IC_result[i,j,k] += BIC
-                        else:
-                            IC_result[i,j,k] += AIC
-                            
+                    if cv_type == 'AICc': # TODO: change to just = instead of += ?
+                        IC_result[i,j,k] += AICc
+                    elif cv_type == 'BIC':
+                        IC_result[i,j,k] += BIC
+                    else:
+                        IC_result[i,j,k] += AIC
+
         # Min IC value (first occurrence)
         ind = np.unravel_index(np.argmin(IC_result, axis=None), IC_result.shape)
         cell_type = kwargs['cell_type'][ind[0]]
         activation = kwargs['activation'][ind[1]]
         RNN_layers = kwargs['RNN_layers'][ind[2]]
-       
+
         prediction_train, prediction_val, prediction_test, AICs, train_loss_final, val_loss_final, test_loss_final = RNN.timeseries_RNN_feedback_single_train(X, y, X_val, y_val, X_test, y_test,
                 kwargs['val_ratio'], cell_type, activation, RNN_layers, kwargs['batch_size'], kwargs['epoch_overlap'], kwargs['num_steps'], kwargs['learning_rate'], kwargs['lambda_l2_reg'],
                 kwargs['num_epochs'], kwargs['input_prob'], kwargs['output_prob'], kwargs['state_prob'], input_prob_test, output_prob_test, state_prob_test, kwargs['max_checks_without_progress'],
                 kwargs['epoch_before_val'], kwargs['save_location'], kwargs['plot'])
-        
+
         hyperparams = {}
         hyperparams['cell_type'] = cell_type
         hyperparams['activation'] = activation
