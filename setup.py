8cd4835cad174f403086c58a1e301a0e2b126aaa
PedroSeber <pedroseber@hotmail.com>
2023-07-24 15:34:51 -0400

    Setup: added pytorch; made versions less restrictive

--- setup.py
+++ setup.py
@@ -5,24 +5,24 @@ setup(
     author = 'Original by Vicky Sun, modified by Pedro Seber',
     author_email = 'pseber@mit.edu',
     license = 'MIT',
-    version = '1.1.0',
+    version = '1.2.0',
     description = 'A software for predictive modeling.',
     zip_safe = False,
     packages = find_packages(where = 'Code-SPA', include = ['SPA.py']),
     package_dir = {'': 'Code-SPA'},
     # Note: other versions of these packages most likely still work, but we have not determined what are the minimum version requirements
     install_requires = [
-        'notebook==6.4.11',
-        'scikit-learn==1.0.2',
-        'matplotlib==3.5.1',
-        'pandas==1.4.2',
-        'statsmodels==0.13.2',
-        'seaborn==0.11.2',
-        'rpy2==3.5.1',
-        'openpyxl==3.0.9',
-        'xlrd==2.0.1',
-        'tensorflow==2.8.1',
-        'ace-cream>=0.4'
+        'notebook>=6.4.11',
+        'scikit-learn>=1.0.2',
+        'matplotlib>=3.5.1',
+        'pandas>=1.4.2',
+        'statsmodels>=0.13.2',
+        'seaborn>=0.11.2',
+        'openpyxl>=3.0.9',
+        'xlrd>=2.0.1',
+        'tensorflow>=2.8.1',
+        'ace-cream>=0.4',
+        'torch>=2.0.0'
         ],
     )
 
