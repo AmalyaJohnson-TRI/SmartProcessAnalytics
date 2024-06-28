dataset_property_new: fixed a off-by-one indexing error

Code-SPA/dataset_property_new.py
@@ -33,7 +33,7 @@ def nonlinearity_assess(X, y, plot = True, cat = None, alpha = 0.01, difference 
         int, whether there is nonlinearity in dataset
     """
     poly, _, _ = _feature_trans(X, degree = 2, interaction = True, trans_type = 'simple_interaction')
-    Bi = poly[:, X.shape[1]+1:] # Just the interaction terms and not the intercept or x0, x1, ..., xN terms
+    Bi = poly[:, X.shape[1]:] # Just the interaction terms and not the intercept or x0, x1, ..., xN terms
   
     # Nonlinearity by linear correlation, quadratic test, and maximal correlation
     m = np.shape(X)[1]